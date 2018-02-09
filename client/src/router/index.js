e2503541a11c546110b2bb16793f1429f3be0415
Abram Villanueva <apvillan00@gmail.com>
2018-02-09 15:56:49 -0800

    Rough UI for registration
    only html

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Registration from '@/components/HomeRegistration'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+
+    {
+      path: '/registration',
+      name: 'Registration',
+      component: Registration
     }
   ]
 })
