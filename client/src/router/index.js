0b625bf0c12970f1f6521aa812e574d0269ac52c
Abram Villanueva <apvillan00@gmail.com>
2018-02-10 16:23:02 -0800

    Registration update

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Vuelidate from 'vuelidate'
 import Home from '@/components/Home'
 import Registration from '@/components/HomeRegistration'
 
 Vue.use(Router)
+Vue.use(Vuelidate)
 
 export default new Router({
   routes: [
